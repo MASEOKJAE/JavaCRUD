eea5d9787f893008e06d3daf7168e528c9999399
MASEOKJAE <maasj7514@gmail.com>
2022-09-04 21:06:27 +0900

    3 Commit

--- src/main/java/SJProject/assignment1/WordCRUD.java
+++ src/main/java/SJProject/assignment1/WordCRUD.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class WordCRUD{
-    private static ArrayList<Word> wordGroup = new ArrayList<>();
+    private static ArrayList<Word> wordGroup = new ArrayList<>(); // 단어들을 관리할 단어장
     private String[] creatData = new String[3];
 
     public WordCRUD() {
@@ -16,14 +16,14 @@ public class WordCRUD{
     public String[] getCreatData() {
         return creatData;
     }
-
+    // Word class의 객체로 단어들을 관리
     public void addList() {
         String[] data = new String[3]; // 3가지 데이터 저장
         data = getCreatData();
         Word wData = new Word(Integer.parseInt(data[0]), data[1], data[2]); // Word Class에 단어 정보 객체로 저장
         wordGroup.add(wData); // word 정보 입력 순서대로 객체로 저장
     }
-
+    // 새로운 단어 추가
     public int create() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("난이도(1,2,3) & 새 단어 입력 : ");
@@ -48,7 +48,7 @@ public class WordCRUD{
         String newMeaning = bf.readLine();
         creatData[2] = newMeaning;
     }
-
+    // 단어장에 있는 모든 단어들
     public void listAll() {
         for(int i=0; i<wordGroup.size(); i++) {
             int stars = wordGroup.get(i).getLevel();
@@ -60,6 +60,19 @@ public class WordCRUD{
                 System.out.print("***  ");
             System.out.print(wordGroup.get(i).getWord() + "   ");
             System.out.println(wordGroup.get(i).getDef());
+        }
+        System.out.println();
+    }
+    // 선택한 수준으로 필터링 한 단어들
+    public void listLevel(int level) {
+        for(int i=0; i<wordGroup.size(); i++) {
+            int stars = wordGroup.get(i).getLevel();
+            if(stars == level) { // 선택한 수준과 현재 순서의 단어의 수준이 같음
+                for(int j=0; j<level; j++) // 수준만큼 *을 출력
+                    System.out.print("*");
+                System.out.print("    " + wordGroup.get(i).getWord() + "   ");
+                System.out.println(wordGroup.get(i).getDef());
+            }
         }
         System.out.println();
     }
